1d3921fb21ef788563707beb3228516d3b4a728a
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2022-12-03 21:46:13 +0200

    Update PostProcess.cs

--- Assets/_PackageRoot/Editor/Scripts/PostProcess.cs
+++ Assets/_PackageRoot/Editor/Scripts/PostProcess.cs
@@ -7,11 +7,21 @@ public class iOSPodsBitcodePostProcess
     [PostProcessBuildAttribute(45)] //must be between 40 and 50 to ensure that it's not overriden by Podfile generation (40) and that it's added before "pod install" (50)
     private static void PostProcessBuild_iOS(BuildTarget target, string buildPath)
     {
+        const string PodfileEnding = "post_install do |installer|\r\n  installer.pods_project.targets.each do |target|\r\n    target.build_configurations.each do |config|\r\n      config.build_settings['ENABLE_BITCODE'] = '{0}'\r\n    end\r\n  end\r\nend";
+
         if (target == BuildTarget.iOS)
         {
             using (StreamWriter sw = File.AppendText(buildPath + "/Podfile"))
             {
-                sw.WriteLine("post_install do |installer|\r\n  installer.pods_project.targets.each do |target|\r\n    target.build_configurations.each do |config|\r\n      config.build_settings['ENABLE_BITCODE'] = 'YES'\r\n    end\r\n  end\r\nend");
+#if PODS_BITCODE_NO
+                var bitcodeStatus = "NO";
+#elif PODS_BITCODE_YES
+                var bitcodeStatus = "YES";
+#else
+                var bitcodeStatus = "YES";
+#endif
+
+                sw.WriteLine(string.Format(PodfileEnding, bitcodeStatus));
             }
         }
     }
